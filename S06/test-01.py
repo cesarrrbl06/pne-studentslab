564b2ef9e68724858b117a9f2ed061357c06f913
cesarrbl <cm.robalo.2023@alumnos.urjc.es>
2024-02-13 16:42:31 +0100

    commit

--- S06/test-01.py
+++ S06/test-01.py
@@ -1,24 +1,20 @@
 class Seq:
-        """A class for representing sequences"""
+    """A class for representing sequences"""
 
-        def __init__(self, strbases):
-            # Initialize the sequence with the value
-            # passed as argument when creating the object
-            self.strbases = strbases
-            print("New sequence created!")
+    def __init__(self, strbases):
+        # Initialize the sequence with the value
+        # passed as argument when creating the object
+        self.strbases = strbases
+        print("New sequence created!")
 
-        def __str__(self):
-            """Method called when the object is being printed"""
-            # -- We just return the string with the sequence
-            return self.strbases
+    def __str__(self):
+        """Method called when the object is being printed"""
+        # -- We just return the string with the sequence
+        return self.strbases
 
-        def len(self):
-            """Calculate the length of the sequence"""
-            return len(self.strbases)
-
-
-
-
+    def len(self):
+        """Calculate the length of the sequence"""
+        return len(self.strbases)
 
 
 class Gene(Seq):
@@ -26,8 +22,8 @@ class Gene(Seq):
        All the objects of class Gene will inherit
        the methods from the Seq class
     """
-    def __init__(self, strbases, name=""):
 
+    def __init__(self, strbases, name=""):
         # -- Call first the Seq initializer and then the
         # -- Gene init method
         super().__init__(strbases)
@@ -39,7 +35,6 @@ class Gene(Seq):
         return self.name + "-" + self.strbases
 
 
-
 # --- Main program
 s1 = Seq("AGTACACTGGT")
 g = Gene("CGTAAC", "FRAT1")
@@ -48,13 +43,6 @@ g = Gene("CGTAAC", "FRAT1")
 print(f"Sequence 1: {s1}")
 print(f"Gene: {g}")
 
-
-
 import termcolor
 
 termcolor.cprint("Hey! this is printed in cyan!", 'cyan')
-
-
-
-
-
