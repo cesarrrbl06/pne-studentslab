ca6c1965b5c0af80b3ec24f84925c18fee9582c1
cesarrbl <cm.robalo.2023@alumnos.urjc.es>
2024-02-13 14:51:41 +0100

    commit

--- P00/seq0.py
+++ P00/seq0.py
@@ -1,4 +1,17 @@
 def seq_ping():
     print("OK")
 
+
+from pathlib import Path
+
+
 def seq_read_fasta(filename):
+    file_contents = Path(filename).read_text()
+    list_contents = file_contents.split('\n')
+    dna_sequence = ''.join(list_contents[1:])  # Join all lines except the header
+    return dna_sequence
+
+
+filename = "Amphiprion_ocellaris_U5_sequence.txt"
+dna_sequence = seq_read_fasta(filename)
+print(dna_sequence)
