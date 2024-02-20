fd4e8223663c2e461b78d694282abdf8c0b68dd4
cesarrrbl06 <cm.robalo.2023@alumnos.urjc.es>
2024-02-20 10:23:37 +0100

    hello

---
+++ P01/e7.py
@@ -0,0 +1,41 @@
+from Seq1 import Seq
+
+class MySeq(Seq):
+    def __init__(self, strbases):
+        self.strbases = strbases  # Do not call Seq's __init__ method
+        if not all(base in 'ACTG' for base in strbases):
+            print("INVALID sequence!")
+            self.strbases = "ERROR"
+        elif len(strbases) == 0:
+            print("NULL sequence created")
+            self.strbases = "NULL"
+        else:
+            print("New sequence created!")
+        self.count_bases()
+
+    def count_bases(self):
+        bases = ['A', 'C', 'T', 'G']
+        self.bases_count = {base: self.strbases.count(base) for base in bases}
+
+    def reverse(self):
+        if self.strbases in ["NULL", "ERROR"]:
+            return self.strbases
+        else:
+            return self.strbases[::-1]
+
+    def __str__(self):
+        return self.strbases
+
+    def len(self):
+        return len(self.strbases)
+
+print("-----| Practice 1, Exercise 7 |------")
+seq1 = MySeq("")
+seq2 = MySeq("ACTGA")
+seq3 = MySeq("TATXC")
+
+for i, seq in enumerate([seq1, seq2, seq3], 1):
+    print(f"Sequence {i}: (Length: {seq.len()}) {seq}")
+    print(f"  Bases: {seq.bases_count}")
+    print(f"  Rev:   {seq.reverse()}")
+    print("\n")
