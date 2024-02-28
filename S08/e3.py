1e3527c8f76bc6eb5bb3473e69b3809b319b3900
cesarrbl <cm.robalo.2023@alumnos.urjc.es>
2024-02-28 15:29:29 +0100

    commit

--- S08/e3.py
+++ S08/e3.py
@@ -5,12 +5,22 @@ PORT = 8081
 IP = "192.168.124.179" # depends on the computer the server is running
 
 while True:
-  # -- Ask the user for the message
+    # First, create the socket
+    # We will always use these parameters: AF_INET y SOCK_STREAM
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-  # -- Create the socket
+    # establish the connection to the Server (IP, PORT)
+    s.connect((IP, PORT))
 
-  # -- Establish the connection to the Server
+    # Send data. No strings can be sent, only bytes
+    # It necessary to encode the string into bytes
+    s.send(str.encode("HELLO FROM THE CLIENT!!!"))
 
-  # -- Send the user message
+    # Receive data from the server
+    msg = s.recv(2048)
+    print("MESSAGE FROM THE SERVER:\n")
+    print(msg.decode("utf-8"))
 
-  # -- Close the socket
+# Closing the socket
+    s.close()
+
