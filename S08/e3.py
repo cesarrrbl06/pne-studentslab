e19083d648e5ec27565b051a82706942b296300d
cesarrbl <cm.robalo.2023@alumnos.urjc.es>
2024-02-28 16:00:21 +0100

    commit

--- S08/e3.py
+++ S08/e3.py
@@ -2,7 +2,7 @@ import socket
 
 # SERVER IP, PORT
 PORT = 8081
-IP = "212.128.255.93"  # depends on the computer the server is running
+IP = "212.128.255.94"  # depends on the computer the server is running
 
 while True:
     # First, create the socket
