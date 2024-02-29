423f48398a02d1d6f83e288180e6a6f693075c4d
cesarrrbl06 <cm.robalo.2023@alumnos.urjc.es>
2024-02-29 19:04:18 +0100

    hello

--- P02/e5.py
+++ P02/e5.py
@@ -1,9 +1,38 @@
 from seq0 import *
 from Client0 import Client
+from Seq1 import *
+
+PRACTICE = 2
+EXERCISE = 5
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "192.168.1.35"  # your IP address
+PORT = 8081
+
+# -- Create a client object
+c = Client(IP, PORT)
+
+# -- Test the ping method
+print(c)
 
 genes = ["FRAT1"]
 for gene in genes:
     seq = seq_read_fasta(f"{gene}.txt")
 
 
-for i in seq:
+fragments = [seq[i:i+10] for i in range(0, len(seq), 10)]
+print("Gene FRAT1:" + seq)
+for i, fragment in enumerate(fragments[:5]):
+    print(f"Fragment {i+1}: {fragment}")
+    response = c.talk(fragment)
+
+
+
+
+
+
+
+
+
