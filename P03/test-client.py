42ad0c2e53d3c449b35948b1d9ce7f647160d3e0
cesarrbl <cm.robalo.2023@alumnos.urjc.es>
2024-03-11 14:50:10 +0100

    commit

--- P03/test-client.py
+++ P03/test-client.py
@@ -1,17 +1,19 @@
+
+from seq0 import *
 from Client0 import *
+from Seq1 import *
 
 PRACTICE = 2
-EXERCISE = 1
+EXERCISE = 5
 
 print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
 
 # -- Parameters of the server to talk to
-IP = "127.0.0.1" # your IP address
+IP = "127.0.0.1"  # your IP address
 PORT = 8080
 
 # -- Create a client object
 c = Client(IP, PORT)
 
 # -- Test the ping method
-c.ping()
-
+print(c)
